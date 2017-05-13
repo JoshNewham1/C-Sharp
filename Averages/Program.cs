81bc21abfcf6e06b1ccbdfd5fc6e10ae16a6298a
Josh Newham <joshnewham@live.com>
2017-05-13 21:04:10 +0100

    Version 1.4 - Added input/output of a number list via a text file stored in C:\User\Documents.

--- Averages/Program.cs
+++ Averages/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace ConsoleApplication1
 {
@@ -15,6 +16,12 @@ namespace ConsoleApplication1
             double tempMean = 0;
             int middlenumber;
             double median;
+            string writetotextfile;
+            string windowsusername = Environment.UserName;
+            string readfromtextfile;
+            string line;
+            string newline;
+
             while (true) {
                 Console.Clear();
                 Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=");
@@ -22,8 +29,24 @@ namespace ConsoleApplication1
                 Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=");
                 Console.WriteLine("Which average would you like to calculate? ");
                 typeofaverage = Console.ReadLine();
-                Console.WriteLine("Please specify which numbers you would like to find the average of seperated by commas. ");
-                numberlist = Console.ReadLine();
+                Console.WriteLine("Would you like to input your numbers from a text file? ");
+                readfromtextfile = Console.ReadLine();
+                if (readfromtextfile == "y" || readfromtextfile == "Y")
+                {
+                    StreamReader sr = new StreamReader("C:\\Users\\" + windowsusername + "\\Documents\\AverageList.txt");
+                    line = sr.ReadLine();
+                    if (line.EndsWith(","))
+                    {
+                        line = line.Remove(line.Length - 1); // If there is a comma at the end of the text file, this removes it so it can be interpretted properly.
+                    }
+                    sr.Close();
+                    numberlist = line;
+                }
+                else
+                {
+                    Console.WriteLine("Please specify which numbers you would like to find the average of seperated by commas. ");
+                    numberlist = Console.ReadLine();
+                }
                 string[] split = numberlist.Split(',');
                 int[] intsplit = split.Select(int.Parse).ToArray();
                 if (typeofaverage == "mean" || typeofaverage == "Mean") {
@@ -56,6 +79,22 @@ namespace ConsoleApplication1
                                        .Select(g => g.Key) // Selecting the key of the group
                                        .First(); // Taking the most frequent value
                     Console.WriteLine("The mode of the list is " + mode);
+                }
+                Console.WriteLine("Would you like to save your list to a text file for later reference?");
+                writetotextfile = Console.ReadLine();
+                if (writetotextfile == "y" || writetotextfile == "Y")
+                {
+                    for (int i = 0; i < split.Length; i++)
+                    {
+                        newline = $"{split[i]},";
+                        FileStream fs = new FileStream("C:\\Users\\" + windowsusername + "\\Documents\\AverageList.txt", FileMode.OpenOrCreate);
+                        StreamWriter str = new StreamWriter(fs);
+                        str.BaseStream.Seek(0, SeekOrigin.End);
+                        str.Write(newline);
+                        str.Flush();
+                        str.Close();
+                        fs.Close();
+                    }
                 }
                 Console.WriteLine("Type 'Q' to quit or press enter to calculate another average. ");
                 endofprogram = Console.ReadLine();
