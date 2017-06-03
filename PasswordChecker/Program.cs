87da3e3bde7621cc5974952806f96a4b317c27d8
Josh Newham <joshnewham@live.com>
2017-06-03 17:42:34 +0100

    Version 1.6 - Whenever the user is entering a password, it shows up as asterisks (*) instead of plain text. New menu system and exit option. Commented all of the ReadPassword() method for easier reading.

--- PasswordChecker/Program.cs
+++ PasswordChecker/Program.cs
@@ -23,12 +23,11 @@ namespace PasswordChecker
 
         static void Main(string[] args)
         {
-            string loadfromtextfile;
+            string menuoption;
             string password;
             string confirmedPassword;
             string windowsusername = Environment.UserName;
             string specifiedUsername;
-            string oldPassword;
             string newPassword;
             
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
@@ -36,34 +35,42 @@ namespace PasswordChecker
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
             while (true)
             {
-                Console.WriteLine("Would you like to load from a text file?");
-                loadfromtextfile = Console.ReadLine();
+                Console.WriteLine("Would you like to:");
+                Console.WriteLine("(C)heck a password and determine its strength");
+                Console.WriteLine("(R)eset a password in the text document");
+                Console.WriteLine("(E)xit the program");
+                menuoption = Console.ReadLine();
 
-                if (loadfromtextfile == "n" || loadfromtextfile == "N")
+                if (menuoption == "e" || menuoption == "E")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (menuoption == "c" || menuoption == "C")
                 {
                     Console.WriteLine("Please input a password");
-                    password = Console.ReadLine();
+                    password = ReadPassword();
 
                     while (password.Length < 8)
                     {
                         Console.WriteLine("Invalid password - please use 8 or more characters");
-                        password = Console.ReadLine();
+                        password = ReadPassword();
                     }
 
                     while (password.Any(char.IsUpper) != true || password.Any(char.IsLower) != true)
                     {
                         Console.WriteLine("Invalid password - please use a mixture of upper and lower case characters");
-                        password = Console.ReadLine();
+                        password = ReadPassword();
                     }
 
 
                     Console.WriteLine("Please confirm your password");
-                    confirmedPassword = Console.ReadLine();
+                    confirmedPassword = ReadPassword();
 
                     while (confirmedPassword != password)
                     {
                         Console.WriteLine("These passwords do not match. Please try again.");
-                        confirmedPassword = Console.ReadLine();
+                        confirmedPassword = ReadPassword();
                     }
 
                     Console.WriteLine("The password you have entered is valid.");
@@ -73,7 +80,7 @@ namespace PasswordChecker
                     Console.ReadLine();
                     Console.Clear();
                 }
-                if (loadfromtextfile == "y" || loadfromtextfile == "Y")
+                if (menuoption == "r" || menuoption == "R")
                 {
                     StreamReader sr = new StreamReader("C:\\Users\\" + windowsusername + "\\Documents\\PasswordReset.txt");
                     Console.WriteLine("Which username would you like to modify the password of?");
@@ -86,15 +93,15 @@ namespace PasswordChecker
                         if (specifiedUsername == usernameSplit[1]) // Checks if the user has typed a valid username
                         {
                             Console.WriteLine("Please type the password you would like to change");
-                            oldPassword = Console.ReadLine();
+                            string oldPassword = ReadPassword();
                             string nextLine = sr.ReadLine();
                             string[] nextLineSplit = nextLine.Split(' ');
                             if (oldPassword == nextLineSplit[1]) // Checks if the user has typed the password below that username
                             {
                                 Console.WriteLine("What would you like your new password to be?");
-                                newPassword = Console.ReadLine();
+                                newPassword = ReadPassword();
                                 Console.WriteLine("Please confirm your new password");
-                                confirmedPassword = Console.ReadLine();
+                                confirmedPassword = ReadPassword();
                                 if (newPassword == confirmedPassword)
                                 {
                                     entireFile = File.ReadAllText("C:\\Users\\" + windowsusername + "\\Documents\\PasswordReset.txt");
@@ -133,6 +140,42 @@ namespace PasswordChecker
 
             }
         }
+        public static string ReadPassword() // Method to place asterisks instead of password
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true); // Reads which keys the user presses
+            while (info.Key != ConsoleKey.Enter) // While the key isn't enter
+            {
+                if (info.Key != ConsoleKey.Backspace) // And while the key isn't backspace
+                {
+                    Console.Write("*"); // Write an asterisk to the screen
+                    password += info.KeyChar; // Add the character to a string
+                }
+                else if (info.Key == ConsoleKey.Backspace) // If the key is backspace
+                {
+                    if (!string.IsNullOrEmpty(password)) // And the password isn't empty
+                    {
+                        // Remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // Get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // Move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // Replace it with space
+                        Console.Write(" ");
+                        // Move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+
+            // Adds a new line after the user has pressed enter
+            Console.WriteLine();
+            return password;
+        }
+    
+
         public static PasswordScore CheckStrength(string password)
         {
             int score = 0;
