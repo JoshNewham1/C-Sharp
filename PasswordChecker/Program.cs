a7b5b550393d60e34c67d4d311f2d3cf46def3a8
Josh Newham <joshnewham@live.com>
2017-06-06 21:04:54 +0100

    Version 1.7 - Implemented encrypting/decrypting of string manually using SHA256 hashes

--- PasswordChecker/Program.cs
+++ PasswordChecker/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Security.Cryptography;
 
 namespace PasswordChecker
 {
@@ -15,11 +16,12 @@ namespace PasswordChecker
         Medium = 2,
         Strong = 3,
         VeryStrong = 4
-    } 
+    }
     class Program
     {
         public static string entireFile;
         private static bool replaceText;
+        private static string input;
 
         static void Main(string[] args)
         {
@@ -29,7 +31,7 @@ namespace PasswordChecker
             string windowsusername = Environment.UserName;
             string specifiedUsername;
             string newPassword;
-            
+
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
             Console.WriteLine("Welcome to Josh's Password Reset Program");
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
@@ -38,10 +40,12 @@ namespace PasswordChecker
                 Console.WriteLine("Would you like to:");
                 Console.WriteLine("(C)heck a password and determine its strength");
                 Console.WriteLine("(R)eset a password in the text document");
-                Console.WriteLine("(E)xit the program");
+                Console.WriteLine("(E)ncrypt a password");
+                Console.WriteLine("(D)ecrypt a password");
+                Console.WriteLine("E(x)it the program");
                 menuoption = Console.ReadLine();
 
-                if (menuoption == "e" || menuoption == "E")
+                if (menuoption == "x" || menuoption == "X")
                 {
                     Environment.Exit(0);
                 }
@@ -114,7 +118,7 @@ namespace PasswordChecker
                             if (nextLine.IndexOf(oldPassword, StringComparison.CurrentCultureIgnoreCase) == 1)
                             {
                                 break;
-                            } 
+                            }
                         }
                     }
                     if (replaceText == true)
@@ -132,7 +136,47 @@ namespace PasswordChecker
                         Console.ReadLine();
                         Console.Clear();
                     }
-                    
+
+
+                }
+
+                if (menuoption == "e" || menuoption == "E")
+                {
+                    Console.WriteLine("Please type the password you would like to encrypt: ");
+                    input = Console.ReadLine();
+                    string keyword = "abcdefghijklmnopqrstuvwxyz";
+                    // Get the bytes of the string
+                    byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+                    byte[] keywordBytes = Encoding.UTF8.GetBytes(keyword);
+
+                    // Hash the password with SHA256
+                    keywordBytes = SHA256.Create().ComputeHash(keywordBytes);
+
+                    byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, keywordBytes);
+
+                    string result = Convert.ToBase64String(bytesEncrypted);
+
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+                if (menuoption == "d" || menuoption == "D")
+                {
+                    Console.WriteLine("Please type the hash created from the encryption: ");
+                    string input = Console.ReadLine();
+                    string keyword = "abcdefghijklmnopqrstuvwxyz";
+                    // Get the bytes of the string
+                    byte[] bytesToBeDecrypted = Convert.FromBase64String(input);
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(keyword);
+                    passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+                    byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+                    string result = Encoding.UTF8.GetString(bytesDecrypted);
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                    Console.Clear();
 
                 }
 
@@ -174,7 +218,73 @@ namespace PasswordChecker
             Console.WriteLine();
             return password;
         }
-    
+
+        public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
 
         public static PasswordScore CheckStrength(string password)
         {
