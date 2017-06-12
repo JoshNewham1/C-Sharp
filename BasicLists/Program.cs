9269a3e64f84a3207d80570a6e89a1c1d6a4fcb7
Josh Newham <joshnewham@live.com>
2017-06-12 18:55:03 +0100

    Version 1.1 - Added a menu system and added the ability to choose what number item they would like to print. Also added an option to exit in the menu.

--- BasicLists/Program.cs
+++ BasicLists/Program.cs
@@ -12,18 +12,24 @@ namespace BasicLists
         {
             string name;
             string order;
+            string elementString;
+            int elementInt;
             List<string> nameList = new List<string>();
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-");
             Console.WriteLine("Josh's Basic List Program");
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-");
-            
+
             while (true)
             {
                 Console.WriteLine("Please type a name to add to the list or type 'l' to view the list");
                 name = Console.ReadLine();
                 if (name == "l" || name == "L")
                 {
-                    Console.WriteLine("Would you like to print them out in (o)rder or in (r)everse?");
+                    Console.WriteLine("Would you like to:");
+                    Console.WriteLine("Print them out in (o)rder");
+                    Console.WriteLine("Print them out in (r)everse");
+                    Console.WriteLine("(C)hoose which element to print out");
+                    Console.WriteLine("E(x)it the program");
                     order = Console.ReadLine().ToUpper();
                     if (order == "O")
                     {
@@ -40,6 +46,20 @@ namespace BasicLists
                             Console.WriteLine(nameList[i]);
                         }
                     }
+                    if (order == "C")
+                    {
+                        Console.WriteLine("Which element would you like to print?");
+                        elementString = Console.ReadLine();
+                        elementInt = Int32.Parse(elementString) - 1; // Add one because computers count from 0
+                        Console.WriteLine(nameList[elementInt]);
+                    }
+                    if (order == "X")
+                    {
+                        Environment.Exit(9);
+                    }
+                    Console.WriteLine("Press enter to continue adding to the list...");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 else
                 {
