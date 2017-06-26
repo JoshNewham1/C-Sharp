72ea4d333667d390afa965c88f8b1c11fa81cea9
JoshNewham1 <joshnewham@live.com>
2017-06-26 20:51:20 +0100

    Version 1.0 - Implemented a word counter using Regular Expressions and counting how many times the string matched the rules. Added a menu system and a quit option. The word counting takes place in a separate method.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menuOption;
+            string input;
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("Josh's Word Counter");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-");
+
+            while (true)
+            {
+                Console.WriteLine("-=-=");
+                Console.WriteLine("Menu");
+                Console.WriteLine("-=-=");
+                Console.WriteLine("(A)dd words and sentences to be counted");
+                Console.WriteLine("(R)ead a text file of sentences to count");
+                Console.WriteLine("E(x)it the program");
+                menuOption = Console.ReadLine().ToUpper();
+
+                while (menuOption == "A")
+                {
+                    Console.WriteLine("Please write words that you would like to be counted or type 'M' to return to the menu.");
+                    input = Console.ReadLine();
+                    if (input.ToUpper() == "M")
+                    {
+                        menuOption = "M";
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine(CountWords(input) + " words");
+                    }
+                    
+                }
+
+                if (menuOption == "X")
+                {
+                    Environment.Exit(9);
+                }
+            }
+        }
+
+        public static int CountWords (string input)
+        {
+            MatchCollection words = Regex.Matches(input, @"[\S]+");
+            return words.Count;
+        }
+    }
+}
