40bac258f37ba6c3d5634cf4ae316bed2761a7bb
JoshNewham1 <joshnewham@live.com>
2017-06-28 16:48:49 +0100

    Version 1.2 - Moved all of the vowel counting in to a separate method.

--- VowelCounter/Program.cs
+++ VowelCounter/Program.cs
@@ -7,16 +7,17 @@ namespace VowelCounter
 {
     class Program
     {
+        private static int vowelCount;
+        private static int aCount = 0;
+        private static int eCount = 0;
+        private static int iCount = 0;
+        private static int oCount = 0;
+        private static int uCount = 0;
         static void Main(string[] args)
         {
             string menuOption;
             string input;
-            int vowelCount;
-            int aCount = 0;
-            int eCount = 0;
-            int iCount = 0;
-            int oCount = 0;
-            int uCount = 0;
+            
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=");
             Console.WriteLine("Josh's Vowel Counter");
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=");
@@ -41,30 +42,7 @@ namespace VowelCounter
                     }
                     else
                     {
-                        vowelCount = input.Count("aeiouAEIOU".Contains);
-                        for (int i = 0; i < input.Length; i++)
-                        {
-                            if (input[i] == 'a' || input[i] == 'A')
-                            {
-                                aCount++;
-                            }
-                            if (input[i] == 'e' || input[i] == 'E')
-                            {
-                                eCount++;
-                            }
-                            if (input[i] == 'i' || input[i] == 'I')
-                            {
-                                iCount++;
-                            }
-                            if (input[i] == 'o' || input[i] == 'O')
-                            {
-                                oCount++;
-                            }
-                            if (input[i] == 'u' || input[i] == 'u')
-                            {
-                                uCount++;
-                            }
-                        }
+                        CountVowels(input);
                         Console.WriteLine("There are " + vowelCount + " vowels in the sentence");
                         Console.WriteLine("Vowels used:");
                         if (aCount > 0)
@@ -95,6 +73,40 @@ namespace VowelCounter
                     Environment.Exit(9);
                 }
             }
+        }
+
+        public static int CountVowels (string input)
+        {
+            aCount = 0; // Resets
+            eCount = 0; // All
+            iCount = 0; // Of
+            oCount = 0; // The
+            uCount = 0; // Counts
+            vowelCount = input.Count("aeiouAEIOU".Contains); // Count how many times the user's input contains the vowels
+            for (int i = 0; i < input.Length; i++) // For each character in input
+            {
+                if (input[i] == 'a' || input[i] == 'A') // Check if it is an 'a'
+                {
+                    aCount++; // Increments the counter
+                }
+                if (input[i] == 'e' || input[i] == 'E') // Check if it is an 'e'
+                {
+                    eCount++; // Increments the counter
+                }
+                if (input[i] == 'i' || input[i] == 'I') // Check if it is an 'i'
+                {
+                    iCount++; // Increments the counter
+                }
+                if (input[i] == 'o' || input[i] == 'O') // Check if it is an 'o'
+                {
+                    oCount++; // Increments the counter
+                }
+                if (input[i] == 'u' || input[i] == 'u') // Check if it is an 'u'
+                {
+                    uCount++; // Increments the counter
+                }
+            }
+            return vowelCount; // Returns the vowel count to the program
         }
     }
 }
