109875ff4a11a282199113ce3810cd41915aa9d8
JoshNewham1 <joshnewham@live.com>
2017-06-28 07:22:30 +0100

    Version 1.0 - Program will count the number of vowels using String.Count("aeiouAEIOU".Contains) as it seemed to be the quickest, most efficient way of counting how many vowels in total. I have used a slightly different approach when counting how many of the individual vowels there were. Although it might not be the most efficient way, I found that using a for loop to iterate through the string and then a number of if statements inside the for loop to be the easiest to code and the easiest to read.

---
+++ VowelCounter/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menuOption;
+            string input;
+            int vowelCount;
+            int aCount = 0;
+            int eCount = 0;
+            int iCount = 0;
+            int oCount = 0;
+            int uCount = 0;
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("Josh's Vowel Counter");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=");
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-=-=");
+                Console.WriteLine("Menu");
+                Console.WriteLine("-=-=");
+                Console.WriteLine("Would you like to:");
+                Console.WriteLine("(A)dd sentences manually");
+                Console.WriteLine("E(x)it the program");
+
+                menuOption = Console.ReadLine().ToUpper();
+
+                while (menuOption == "A")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please input words or sentences to count the vowels of. Press 'M' to exit to the menu.");
+                    input = Console.ReadLine();
+                    if (input.ToUpper() == "M")
+                    {
+                        menuOption = "M";
+                    }
+                    else
+                    {
+                        vowelCount = input.Count("aeiouAEIOU".Contains);
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            if (input[i] == 'a' || input[i] == 'A')
+                            {
+                                aCount++;
+                            }
+                            if (input[i] == 'e' || input[i] == 'E')
+                            {
+                                eCount++;
+                            }
+                            if (input[i] == 'i' || input[i] == 'I')
+                            {
+                                iCount++;
+                            }
+                            if (input[i] == 'o' || input[i] == 'O')
+                            {
+                                oCount++;
+                            }
+                            if (input[i] == 'u' || input[i] == 'u')
+                            {
+                                uCount++;
+                            }
+                        }
+                        Console.WriteLine("There are " + vowelCount + " vowels in the sentence");
+                        Console.WriteLine("Vowels used:");
+                        if (aCount > 0)
+                        {
+                            Console.WriteLine("A: " + aCount);
+                        }
+                        if (eCount > 0)
+                        {
+                            Console.WriteLine("E: " + eCount);
+                        }
+                        if (iCount > 0)
+                        {
+                            Console.WriteLine("I: " + iCount);
+                        }
+                        if (oCount > 0)
+                        {
+                            Console.WriteLine("O: " + oCount);
+                        }
+                        if (uCount > 0)
+                        {
+                            Console.WriteLine("U: " + uCount);
+                        }
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
