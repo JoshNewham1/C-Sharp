ea4fc066ecca9431da011e19e0ad093d1125624b
Josh Newham <joshnewham@live.com>
2017-06-29 20:36:12 +0100

    Version 1.1 - Added conversion between units of mass.

--- UnitConverter/Program.cs
+++ UnitConverter/Program.cs
@@ -33,6 +33,9 @@ namespace UnitConverter
             string volumeFrom;
             string volumeTo;
             decimal finalVolume;
+            string massFrom;
+            string massTo;
+            decimal finalMass;
             Dictionary<string, decimal> volumeConversion = new Dictionary<string, decimal>
             {
                 { "ML", 0.001m },
@@ -56,6 +59,39 @@ namespace UnitConverter
                 { "GF",  160m},
                 { "GP",  8m}
             };
+            Dictionary<string, decimal> massConversion = new Dictionary<string, decimal>
+            {
+                { "GK", 0.001m },
+                { "GT",  0.001000000m},
+                { "GS",  0.000157473m},
+                { "GP",  0.00220462m},
+                { "GO", 0.035274m },
+                { "KG",  1000m },
+                { "KT",  0.001m },
+                { "KS",  0.157473m },
+                { "KP", 2.20462m },
+                { "KO",  35.274m },
+                { "TG",  1000000m },
+                { "TK",  1000m },
+                { "TS", 157.473m },
+                { "TP",  2204.62m },
+                { "TO",  35274m},
+                { "SG",  6350.29m},
+                { "SK", 6.35029m },
+                { "ST",  0.00635029m},
+                { "SP",  14m},
+                { "SO",  224m},
+                { "PG",  453.592m},
+                { "PK", 0.453592m },
+                { "PT",  0.000453592m},
+                { "PS",  0.0714286m},
+                { "PO",  16m},
+                { "OG",  28.3495m},
+                { "OK", 0.0283495m },
+                { "OT",  0.00002834952m},
+                { "OS",  0.00446429m},
+                { "OP",  0.0625m}
+            };
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-");
             Console.WriteLine("Josh's Unit Converter");
             Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-");
@@ -66,6 +102,7 @@ namespace UnitConverter
                 Console.WriteLine("(T)emperature");
                 Console.WriteLine("(C)urrency");
                 Console.WriteLine("(V)olume");
+                Console.WriteLine("(M)ass");
 
                 unitToConvert = Console.ReadLine().ToUpper();
                 if (unitToConvert == "T")
@@ -134,30 +171,60 @@ namespace UnitConverter
                 }
                 if (unitToConvert == "V")
                 {
-                    Console.WriteLine("What unit would you like to convert from?");
+                    Console.WriteLine("Which unit would you like to convert from?");
                     Console.WriteLine("(M)illilitres");
                     Console.WriteLine("(L)itres");
                     Console.WriteLine("(P)ints");
                     Console.WriteLine("(F)luid ounce");
                     Console.WriteLine("(G)allon");
-                    volumeFrom = Console.ReadLine();
-                    Console.WriteLine("What unit would you like to convert to?");
+                    volumeFrom = Console.ReadLine().ToUpper();
+                    Console.WriteLine("Which unit would you like to convert to?");
                     Console.WriteLine("(M)illilitres");
                     Console.WriteLine("(L)itres");
                     Console.WriteLine("(P)ints");
                     Console.WriteLine("(F)luid ounce");
                     Console.WriteLine("(G)allon");
-                    volumeTo = Console.ReadLine();
+                    volumeTo = Console.ReadLine().ToUpper();
                     while (volumeFrom == volumeTo)
                     {
-                        Console.WriteLine("Please select a different volume");
+                        Console.WriteLine("Please select a different volume measurement");
                         volumeTo = Console.ReadLine();
                     }
                     Console.WriteLine("How much volume would you like to convert?");
                     stringInput = Console.ReadLine();
                     decimalInput = Convert.ToDecimal(stringInput);
-                    finalVolume = decimalInput * volumeConversion[volumeFrom.ToUpper() + volumeTo.ToUpper()];
+                    finalVolume = decimalInput * volumeConversion[volumeFrom + volumeTo];
                     Console.WriteLine(finalVolume);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                if (unitToConvert == "M")
+                {
+                    Console.WriteLine("Which unit would you like to convert from:");
+                    Console.WriteLine("(G)rams");
+                    Console.WriteLine("(K)ilograms");
+                    Console.WriteLine("(T)onnes");
+                    Console.WriteLine("(S)tone");
+                    Console.WriteLine("(P)ound");
+                    Console.WriteLine("(O)unce");
+                    massFrom = Console.ReadLine().ToUpper();
+                    Console.WriteLine("Which unit would you like to convert to:");
+                    Console.WriteLine("(G)rams");
+                    Console.WriteLine("(K)ilograms");
+                    Console.WriteLine("(T)onnes");
+                    Console.WriteLine("(S)tone");
+                    Console.WriteLine("(P)ound");
+                    Console.WriteLine("(O)unce");
+                    massTo = Console.ReadLine().ToUpper();
+                    while (massFrom == massTo)
+                    {
+                        Console.WriteLine("Please select a different mass measurement");
+                    }
+                    Console.WriteLine("How much mass would you like to convert?");
+                    stringInput = Console.ReadLine();
+                    decimalInput = Convert.ToDecimal(stringInput);
+                    finalMass = decimalInput * massConversion[massFrom + massTo];
+                    Console.WriteLine(finalMass);
                     Console.ReadLine();
                     Console.Clear();
                 }
