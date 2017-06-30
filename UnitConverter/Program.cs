c341e8cdd2bfff7e3e95981a755f1a848296363e
JoshNewham1 <joshnewham@live.com>
2017-06-30 07:15:15 +0100

    Version 1.2 - Commented all of the program and added a Regex.Replace to strip the input for the currency of all special characters (such as £ or $ signs.) Also added a Console.ReadLine() to the while (massFrom == massTo) statement so it will read user input.

--- UnitConverter/Program.cs
+++ UnitConverter/Program.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UnitConverter
 {
     class Program
     {
-        private static decimal exchangeRate;
+        private static decimal exchangeRate; // Defines the exchange rate in the class so it can be viewed and changed by the ConvertCurrency method
 
-        public static decimal ConvertCurrency(string from, string to)
+        public static decimal ConvertCurrency(string from, string to) // ConvertCurrency method
         {
-            var uriString = string.Format("http://finance.yahoo.com/d/quotes.csv?s={0}{1}=X&f=l1", from, to);
-            string response = new WebClient().DownloadString(uriString);
-            exchangeRate = decimal.Parse(response, System.Globalization.CultureInfo.InvariantCulture);
-            return exchangeRate;
+            var uriString = string.Format("http://finance.yahoo.com/d/quotes.csv?s={0}{1}=X&f=l1", from, to); // Specifies the exchange rate from and to a currency from a CSV document on Yahoo's website
+            string response = new WebClient().DownloadString(uriString); // Downloads the exchange rate from the CSV document
+            exchangeRate = decimal.Parse(response, System.Globalization.CultureInfo.InvariantCulture); // Format the exchange rate into a decimal
+            return exchangeRate; // Returns the exchange rate to the rest of the program
         }
         static void Main(string[] args)
         {
@@ -36,9 +37,9 @@ namespace UnitConverter
             string massFrom;
             string massTo;
             decimal finalMass;
-            Dictionary<string, decimal> volumeConversion = new Dictionary<string, decimal>
+            Dictionary<string, decimal> volumeConversion = new Dictionary<string, decimal> // Defines a dictionary with all of the volume conversion numbers in
             {
-                { "ML", 0.001m },
+                { "ML", 0.001m }, // The m after each number signifies it is a decimal
                 { "MP",  0.0017598m},
                 { "MF",  0.033814m},
                 { "MG",  0.000219969m},
@@ -59,9 +60,9 @@ namespace UnitConverter
                 { "GF",  160m},
                 { "GP",  8m}
             };
-            Dictionary<string, decimal> massConversion = new Dictionary<string, decimal>
+            Dictionary<string, decimal> massConversion = new Dictionary<string, decimal> // Defines a dictionary with all of the mass conversion numbers in
             {
-                { "GK", 0.001m },
+                { "GK", 0.001m }, // The m after each number signifies it is a decimal
                 { "GT",  0.001000000m},
                 { "GS",  0.000157473m},
                 { "GP",  0.00220462m},
@@ -98,135 +99,136 @@ namespace UnitConverter
 
             while (true)
             {
-                Console.WriteLine("What would you like to convert:");
+                Console.WriteLine("What would you like to convert:"); // Asks the user what they would like to convert
                 Console.WriteLine("(T)emperature");
                 Console.WriteLine("(C)urrency");
                 Console.WriteLine("(V)olume");
                 Console.WriteLine("(M)ass");
 
-                unitToConvert = Console.ReadLine().ToUpper();
-                if (unitToConvert == "T")
+                unitToConvert = Console.ReadLine().ToUpper(); // Reads this into a variable and capitalises it
+                if (unitToConvert == "T") // If the user wants to convert temperature
                 {
-                    Console.WriteLine("Would you like to:");
+                    Console.WriteLine("Would you like to:"); // Asks the user which way they would like to convert it
                     Console.WriteLine("Convert (c)elsius to fahrenheit");
                     Console.WriteLine("Convert (f)ahrenheit to celsius");
-
-                    temperatureMeasurement = Console.ReadLine().ToUpper();
-                    if (temperatureMeasurement == "C")
+                    temperatureMeasurement = Console.ReadLine().ToUpper(); // Reads this into a variable and capitalises it
+                    if (temperatureMeasurement == "C") // If they want to convert celsius to fahrenheit
                     {
-                        Console.WriteLine("Please specify a number in celsius to convert.");
-                        stringInput = Console.ReadLine();
-                        doubleInput = Convert.ToDouble(stringInput);
-                        fahrenheitConversion = doubleInput * 1.8 + 32;
-                        Console.WriteLine(doubleInput + "°C = " + fahrenheitConversion + "°F");
-                        Console.ReadLine();
-                        Console.Clear();
+                        Console.WriteLine("Please specify a number in celsius to convert."); // Asks the user how many celsius they would like to convert
+                        stringInput = Console.ReadLine(); // Reads the string into a variable
+                        doubleInput = Convert.ToDouble(stringInput); // Converts the string to a double
+                        fahrenheitConversion = doubleInput * 1.8 + 32; // The formula of converting celsius into fahrenheit (x1.8 + 32)
+                        Console.WriteLine(doubleInput + "°C = " + fahrenheitConversion + "°F"); // Prints the converted number
+                        Console.ReadLine(); // Waits for user input
+                        Console.Clear(); // Waits for user input
                     }
-                    if (temperatureMeasurement == "F")
+                    if (temperatureMeasurement == "F") // If the user wants to convert fahrenheit to celsius
                     {
-                        Console.WriteLine("Please specify a number in fahrenheit to convert.");
-                        stringInput = Console.ReadLine();
-                        doubleInput = Convert.ToDouble(stringInput);
-                        celsiusConversion = (doubleInput - 32) / 1.8;
-                        Console.WriteLine(doubleInput + "°F = " + celsiusConversion + "°C");
-                        Console.ReadLine();
-                        Console.Clear();
+                        Console.WriteLine("Please specify a number in fahrenheit to convert."); // Asks the user how many celsius they would like to convert
+                        stringInput = Console.ReadLine(); // Reads the string into a variable
+                        doubleInput = Convert.ToDouble(stringInput); // Converts the string to a double
+                        celsiusConversion = (doubleInput - 32) / 1.8; // The formula of converting fahrenheit into celsius (-32 / 1.8)
+                        Console.WriteLine(doubleInput + "°F = " + celsiusConversion + "°C"); // Prints the converted number
+                        Console.ReadLine(); // Waits for user input
+                        Console.Clear(); // Waits for user input
                     }
                 }
-                if (unitToConvert == "C")
+                if (unitToConvert == "C") // If the user wants to convert currency
                 {
-                    Console.WriteLine("Convert from:");
+                    Console.WriteLine("Convert from:"); // Ask the user what they would like to convert from
                     Console.WriteLine("GBP (British Pound)");
                     Console.WriteLine("USD (U.S Dollar)");
                     Console.WriteLine("EUR (Euro)");
                     Console.WriteLine("JPY (Japanese Yen)");
                     Console.WriteLine("CAD (Canadian Dollar)");
-                    currencyFrom = Console.ReadLine().ToUpper();
+                    currencyFrom = Console.ReadLine().ToUpper(); // Read this into a variable and capitalise it
 
-                    Console.WriteLine("Convert to:");
+                    Console.WriteLine("Convert to:"); // Ask the user what they would like to convert to
                     Console.WriteLine("GBP (British Pound)");
                     Console.WriteLine("USD (U.S Dollar)");
                     Console.WriteLine("EUR (Euro)");
                     Console.WriteLine("JPY (Japanese Yen)");
                     Console.WriteLine("CAD (Canadian Dollar)");
-                    currencyTo = Console.ReadLine().ToUpper();
+                    currencyTo = Console.ReadLine().ToUpper(); // Read this into a variable and capitalise it
 
-                    Console.WriteLine("How much would you like to convert? ");
-                    stringInput = Console.ReadLine();
-                    decimalInput = Convert.ToDecimal(stringInput);
+                    Console.WriteLine("How much would you like to convert? "); // Asks the user how much they would like to convert
+                    stringInput = Console.ReadLine(); // Reads this string into a variable
+                    stringInput = Regex.Replace(stringInput, "[^0-9A-Za-z]+", ""); // Strips the input of any special characters (such as currency signs)
+                    decimalInput = Convert.ToDecimal(stringInput); // Converts it to decimal
 
-                    while (currencyFrom == currencyTo)
+                    while (currencyFrom == currencyTo) // While the currency converting from is the same as the currency converting to
                     {
-                        Console.WriteLine("Please select a different currency");
-                        currencyTo = Console.ReadLine();
+                        Console.WriteLine("Please select a different currency"); // Asks the user to select a different currency
+                        currencyTo = Console.ReadLine(); // Reads their new currency into a variable
                     }
 
-                    ConvertCurrency(currencyFrom, currencyTo);
+                    ConvertCurrency(currencyFrom, currencyTo); // Calls the ConvertCurrency method and passes through the currencyFrom and currencyTo
 
-                    currencyConversion = decimalInput * exchangeRate;
+                    currencyConversion = decimalInput * exchangeRate; // Multiplies the input with the exchange rate
 
-                    Console.WriteLine(Math.Round(currencyConversion, 2) + " " + currencyTo);
-                    Console.ReadLine();
-                    Console.Clear();
+                    Console.WriteLine(Math.Round(currencyConversion, 2) + " " + currencyTo); // Prints out the rounded conversion
+                    Console.ReadLine(); // Waits for user input
+                    Console.Clear(); // Clears the screen
                 }
-                if (unitToConvert == "V")
+                if (unitToConvert == "V") // If the user wants to convert volume
                 {
-                    Console.WriteLine("Which unit would you like to convert from?");
+                    Console.WriteLine("Which unit would you like to convert from?"); // Ask the user what they would like to convert from
                     Console.WriteLine("(M)illilitres");
                     Console.WriteLine("(L)itres");
                     Console.WriteLine("(P)ints");
                     Console.WriteLine("(F)luid ounce");
                     Console.WriteLine("(G)allon");
-                    volumeFrom = Console.ReadLine().ToUpper();
-                    Console.WriteLine("Which unit would you like to convert to?");
+                    volumeFrom = Console.ReadLine().ToUpper(); // Read this into a variable and capitalise it
+                    Console.WriteLine("Which unit would you like to convert to?"); // Ask the user what they would like to convert to
                     Console.WriteLine("(M)illilitres");
                     Console.WriteLine("(L)itres");
                     Console.WriteLine("(P)ints");
                     Console.WriteLine("(F)luid ounce");
                     Console.WriteLine("(G)allon");
-                    volumeTo = Console.ReadLine().ToUpper();
-                    while (volumeFrom == volumeTo)
+                    volumeTo = Console.ReadLine().ToUpper(); // Read this into a variable and capitalise it
+                    while (volumeFrom == volumeTo) // While the volume measurement converting from is the same as the volume measurement converting to
                     {
-                        Console.WriteLine("Please select a different volume measurement");
-                        volumeTo = Console.ReadLine();
+                        Console.WriteLine("Please select a different volume measurement"); // Asks the user to select a different measurement
+                        volumeTo = Console.ReadLine(); // Reads this into a variable
                     }
-                    Console.WriteLine("How much volume would you like to convert?");
-                    stringInput = Console.ReadLine();
-                    decimalInput = Convert.ToDecimal(stringInput);
-                    finalVolume = decimalInput * volumeConversion[volumeFrom + volumeTo];
-                    Console.WriteLine(finalVolume);
-                    Console.ReadLine();
-                    Console.Clear();
+                    Console.WriteLine("How much volume would you like to convert?"); // Asks the user how much they want to convert
+                    stringInput = Console.ReadLine(); // Reads this string in to a variable
+                    decimalInput = Convert.ToDecimal(stringInput); // Converts this to decimal
+                    finalVolume = decimalInput * volumeConversion[volumeFrom + volumeTo]; // Multiplies the decimal by the conversion formula in the dictionary (volumeFrom + volumeTo is how the dictionary is layed out)
+                    Console.WriteLine(finalVolume); // Prints the converted volume
+                    Console.ReadLine(); // Waits for user input
+                    Console.Clear(); // Clears the screen
                 }
-                if (unitToConvert == "M")
+                if (unitToConvert == "M") // If the user wants to convert mass
                 {
-                    Console.WriteLine("Which unit would you like to convert from:");
+                    Console.WriteLine("Which unit would you like to convert from:"); // Ask the user what they would like to convert from
                     Console.WriteLine("(G)rams");
                     Console.WriteLine("(K)ilograms");
                     Console.WriteLine("(T)onnes");
                     Console.WriteLine("(S)tone");
                     Console.WriteLine("(P)ound");
                     Console.WriteLine("(O)unce");
-                    massFrom = Console.ReadLine().ToUpper();
-                    Console.WriteLine("Which unit would you like to convert to:");
+                    massFrom = Console.ReadLine().ToUpper(); // Read this into a variable and capitalise it
+                    Console.WriteLine("Which unit would you like to convert to:"); // Ask the user what they would like to convert to
                     Console.WriteLine("(G)rams");
                     Console.WriteLine("(K)ilograms");
                     Console.WriteLine("(T)onnes");
                     Console.WriteLine("(S)tone");
                     Console.WriteLine("(P)ound");
                     Console.WriteLine("(O)unce");
-                    massTo = Console.ReadLine().ToUpper();
-                    while (massFrom == massTo)
+                    massTo = Console.ReadLine().ToUpper(); // Read this into a variable and capitalise it
+                    while (massFrom == massTo) // While the mass measurement converting from is the same as the mass measurement converting to
                     {
-                        Console.WriteLine("Please select a different mass measurement");
+                        Console.WriteLine("Please select a different mass measurement"); // Asks the user to select a different mass measurement
+                        massTo = Console.ReadLine(); // Reads this into a variable
                     }
-                    Console.WriteLine("How much mass would you like to convert?");
-                    stringInput = Console.ReadLine();
-                    decimalInput = Convert.ToDecimal(stringInput);
-                    finalMass = decimalInput * massConversion[massFrom + massTo];
-                    Console.WriteLine(finalMass);
-                    Console.ReadLine();
-                    Console.Clear();
+                    Console.WriteLine("How much mass would you like to convert?"); // Asks the user how much they would like to convert
+                    stringInput = Console.ReadLine(); // Stores this string in a variable
+                    decimalInput = Convert.ToDecimal(stringInput); // Conevrts the string to a decimal
+                    finalMass = decimalInput * massConversion[massFrom + massTo]; // Multiplies the decimal by the conversion formula in the dictionary (massFrom + massTo is how the dictionary is layed out)
+                    Console.WriteLine(finalMass); // Print the converted mass
+                    Console.ReadLine(); // Wait for user input
+                    Console.Clear(); // Clears the screen
                 }
             }
         }
