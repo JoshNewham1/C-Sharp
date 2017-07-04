655a8f7966d59ed0e01284b98671e8feb8d055dc
JoshNewham1 <joshnewham@live.com>
2017-07-04 07:28:32 +0100

    Version 1.0 - Basic Shopping List with only the item and where the user is buying from implemented. I am using multidimensional lists to store the shopping list because they have no predefined value and I have past experience using them.

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        public class shoppingListFormat
+        {
+            public string item { get; set; }
+            public string supermarket { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var shoppingList = new List<shoppingListFormat>();
+            string menuOption;
+            string specifiedItem;
+            string specifiedSupermarket;
+            int count = 0;
+
+            Console.WriteLine("-=-=-=-=-=-=-");
+            Console.WriteLine("Shopping List");
+            Console.WriteLine("-=-=-=-=-=-=-");
+
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("(A)dd items to your shopping list");
+                Console.WriteLine("(P)rint items from your shopping list");
+
+                menuOption = Console.ReadLine().ToUpper();
+
+                while (menuOption == "A")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please specify the item that you would like to add (or type 'M' to return to the menu)");
+                    specifiedItem = Console.ReadLine();
+                    if (specifiedItem.ToUpper() == "M")
+                    {
+                        menuOption = "M";
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please specify where you would like to buy it from");
+                        specifiedSupermarket = Console.ReadLine();
+
+                        shoppingList.Add(new shoppingListFormat
+                        {
+                            item = specifiedItem,
+                            supermarket = specifiedSupermarket
+                        });
+                    }
+                }
+                if (menuOption == "P")
+                {
+                    Console.Clear();
+                    count = 0;
+                    foreach (var property in shoppingList)
+                    {
+                        count++;
+                        Console.WriteLine("Item " + count + ": " + property.item);
+                        Console.WriteLine("Buying from: " + property.supermarket);
+                        Console.ReadLine();
+                    }
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
