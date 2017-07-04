d310cb262ccf95362db2acc32b1bebefee35b164
JoshNewham1 <joshnewham@live.com>
2017-07-04 20:34:18 +0100

    Version 1.2 - Commented all code and added an exit option

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -9,7 +9,7 @@ namespace ShoppingList
 {
     class Program
     {
-        public class shoppingListFormat
+        public class shoppingListFormat // Creates a new, custom class to use in a list
         {
             public int priority { get; set; }
             public string item { get; set; }
@@ -21,134 +21,139 @@ namespace ShoppingList
 
         static void Main(string[] args)
         {
-            var shoppingList = new List<shoppingListFormat>();
-            string menuOption;
-            string specifiedItem;
-            string specifiedSupermarket;
-            int count = 0;
-            string stringitemBought;
-            bool boolitemBought;
-            string stringWillingPrice;
-            string stringPriority;
-            string stringQuantity;
-            double total = 0;
-            double priceofItem = 0;
+            var shoppingList = new List<shoppingListFormat>(); // Initialises an empty list with the shoppingListFormat format
+            string menuOption; // Define a new string for the users menu option
+            string specifiedItem; // Define a new string for the users specified item
+            string specifiedSupermarket; // Define a new users specified supermarked
+            int count = 0; // Initialises a new integer and sets it to 0
+            string stringitemBought; // Define a new users specified item
+            bool boolitemBought; // Create a boolean for if the item had been bought
+            string stringWillingPrice; // Define a new users specified price
+            string stringPriority; // Define a new users specified priority
+            string stringQuantity; // Define a new users specified quantity
+            double total = 0; // Initialises a new double and sets it to 0
+            double priceofItem = 0; // Initialises a new double and sets it to 0
 
-            Console.WriteLine("-=-=-=-=-=-=-");
-            Console.WriteLine("Shopping List");
+            Console.WriteLine("-=-=-=-=-=-=-"); // Title
+            Console.WriteLine("Shopping List"); // Screen
             Console.WriteLine("-=-=-=-=-=-=-");
 
-            while (true)
+            while (true) // Infinite loop
             {
-                Console.WriteLine("Menu:");
+                Console.WriteLine("Menu:"); // Displays the menu
                 Console.WriteLine("(A)dd items to your shopping list");
                 Console.WriteLine("(P)rint items from your shopping list");
                 Console.WriteLine("(C)alculate the total of the shopping list");
+                Console.WriteLine("E(x)it the program");
 
-                menuOption = Console.ReadLine().ToUpper();
+                menuOption = Console.ReadLine().ToUpper(); // Stores the users option and capitalises it
 
-                while (menuOption == "A")
+                while (menuOption == "A") // While the user is adding items
                 {
-                    Console.Clear();
-                    Console.WriteLine("Please specify the item that you would like to add (or type 'M' to return to the menu)");
-                    specifiedItem = Console.ReadLine();
-                    if (specifiedItem.ToUpper() == "M")
+                    Console.Clear(); // Clear the console
+                    Console.WriteLine("Please specify the item that you would like to add (or type 'M' to return to the menu)"); // Ask the user for an item
+                    specifiedItem = Console.ReadLine(); // Stores this in a variable
+                    if (specifiedItem.ToUpper() == "M") // If the user has entered 'm' or 'M'
                     {
-                        menuOption = "M";
-                        Console.Clear();
+                        menuOption = "M"; // Change the menuOption
+                        Console.Clear(); // Clears the console
                     }
-                    else
+                    else // If the user has entered anything else
                     {
-                        Console.WriteLine("Please specify where you would like to buy it from");
-                        specifiedSupermarket = Console.ReadLine();
-                        Console.WriteLine("Have you bought the item yet?");
-                        stringitemBought = Console.ReadLine().ToUpper();
-                        while (stringitemBought != "Y" && stringitemBought != "N")
+                        Console.WriteLine("Please specify where you would like to buy it from"); // Asks the user where they bought it from
+                        specifiedSupermarket = Console.ReadLine(); // Stores this in a variable
+                        Console.WriteLine("Have you bought the item yet?"); // Asks the user if they bought the item
+                        stringitemBought = Console.ReadLine().ToUpper(); // Stores the response in a variable and capitalises it
+                        while (stringitemBought != "Y" && stringitemBought != "N") // If the user has not entered 'Y' or 'N'
                         {
-                            Console.WriteLine("Please answer with either a 'Y' or 'N'");
-                            stringitemBought = Console.ReadLine().ToUpper();
+                            Console.WriteLine("Please answer with either a 'Y' or 'N'"); // Ask the user to answer correctly
+                            stringitemBought = Console.ReadLine().ToUpper(); // Store this in a variable and capitalise it
                         }
-                        if (stringitemBought == "Y")
+                        if (stringitemBought == "Y") // If the item has been bought
                         {
-                            boolitemBought = true;
+                            boolitemBought = true; // Set the boolean to true
                         }
-                        else
+                        else // If they have entered 'N'
                         {
-                            boolitemBought = false;
+                            boolitemBought = false; // Set the boolean to false
                         }
-                        Console.WriteLine("What is the price you are willing to pay for 1?");
-                        stringWillingPrice = Console.ReadLine();
-                        stringWillingPrice = Regex.Replace(stringWillingPrice, "[^0-9.]", "");
-                        while (stringWillingPrice == "")
+                        Console.WriteLine("What is the price you are willing to pay for 1?"); // Ask the user for their desired price
+                        stringWillingPrice = Console.ReadLine(); // Store this in a variable
+                        stringWillingPrice = Regex.Replace(stringWillingPrice, "[^0-9.]", ""); // Strip everything except numbers and full stops
+                        while (stringWillingPrice == "") // While the price is empty
                         {
-                            Console.WriteLine("Please specify a maximum price.");
-                            stringWillingPrice = Console.ReadLine();
-                            stringWillingPrice = Regex.Replace(stringWillingPrice, "[^0-9.]", "");
+                            Console.WriteLine("Please specify a maximum price."); // Ask the user to specify a price
+                            stringWillingPrice = Console.ReadLine(); // Store this as a variable
+                            stringWillingPrice = Regex.Replace(stringWillingPrice, "[^0-9.]", ""); // Strip this of everything except numbers and full stops
                         }
-                        Console.WriteLine("What is the priority of the item?");
-                        stringPriority = Console.ReadLine();
-                        stringPriority = Regex.Replace(stringPriority, "[^0-9]", "");
-                        Console.WriteLine("What is the quantity of the item?");
-                        stringQuantity = Console.ReadLine();
-                        stringQuantity = Regex.Replace(stringQuantity, "[^0-9]", "");
-                        shoppingList.Add(new shoppingListFormat
+                        Console.WriteLine("What is the priority of the item?"); // Ask the user what the priority is
+                        stringPriority = Console.ReadLine(); // Store this as a variable
+                        stringPriority = Regex.Replace(stringPriority, "[^0-9]", ""); // Strip this of everything except numbers
+                        Console.WriteLine("What is the quantity of the item?"); // Ask for the quantity of the item
+                        stringQuantity = Console.ReadLine(); // Store this as a variable
+                        stringQuantity = Regex.Replace(stringQuantity, "[^0-9]", ""); // Strip this of everything except numbers
+                        shoppingList.Add(new shoppingListFormat // Add all of these values to a list
                         {
-                            priority = Int32.Parse(stringPriority),
-                            item = specifiedItem,
-                            supermarket = specifiedSupermarket,
-                            boughtItem = boolitemBought,
-                            willingPrice = Convert.ToDouble(stringWillingPrice),
-                            quantity = Int32.Parse(stringQuantity)
+                            priority = Int32.Parse(stringPriority), // Priority property of the list is equal to an int parse of stringPriority
+                            item = specifiedItem, // Item is equal to specifiedItem
+                            supermarket = specifiedSupermarket, // Supermarket is equal to specifiedSupermarket
+                            boughtItem = boolitemBought, // boughtItem is equal to boolitemBought
+                            willingPrice = Convert.ToDouble(stringWillingPrice), // willingPrice is equal to stringWillingPrice converted to a double
+                            quantity = Int32.Parse(stringQuantity) // Quantity is equal to an int parse of stringQuantity
                         });
                     }
                 }
-                if (menuOption == "P")
+                if (menuOption == "P") // If the user wants to print the list
                 {
-                    Console.Clear();
-                    List<shoppingListFormat> sortedshoppingList = shoppingList.OrderBy(s=>s.priority).ToList();
-                    count = 0;
-                    foreach (var property in sortedshoppingList)
+                    Console.Clear(); // Clear the console
+                    List<shoppingListFormat> sortedshoppingList = shoppingList.OrderBy(s=>s.priority).ToList(); // Sort the list using the priority property
+                    count = 0; // Reset count to 0
+                    foreach (var property in sortedshoppingList) // For every property in the sorted list
                     {
-                        count++;
-                        Console.WriteLine("Item " + count + ": " + property.item);
-                        Console.WriteLine("Priority: " + property.priority);
-                        Console.WriteLine("Buying from: " + property.supermarket);
-                        if (property.boughtItem == true)
+                        count++; // Increment the count
+                        Console.WriteLine("Item " + count + ": " + property.item); // Print out the item number and the item
+                        Console.WriteLine("Priority: " + property.priority); // Print out the priority of the item
+                        Console.WriteLine("Buying from: " + property.supermarket); // Print out the supermarket
+                        if (property.boughtItem == true) // If the itemBought boolean was true
                         {
-                            Console.WriteLine("Item bought: Yes");
+                            Console.WriteLine("Item bought: Yes"); // Print that the item was bought
                         }
-                        else
+                        else // If the itemBought boolean was true
                         {
-                            Console.WriteLine("Item bought: No");
+                            Console.WriteLine("Item bought: No"); // Print that the item wasn't bought
                         }
-                        Console.WriteLine("Maximum price: £" + property.willingPrice);
-                        Console.WriteLine("Quantity: " + property.quantity);
-                        Console.ReadLine();
+                        Console.WriteLine("Maximum price: £" + property.willingPrice); // Print out the max/desired price
+                        Console.WriteLine("Quantity: " + property.quantity); // Prints out the quantity
+                        Console.ReadLine(); // Wait for user input
                     }
-                    Console.Clear();
+                    Console.Clear(); // Clear the console
                 }
-                if (menuOption == "C")
+                if (menuOption == "C") // If the user wants to calculate a total
                 {
-                    total = 0;
-                    List<shoppingListFormat> sortedshoppingList = shoppingList.OrderBy(s => s.priority).ToList();
-                    foreach (var property in sortedshoppingList)
+                    total = 0; // Reset the total variable
+                    List<shoppingListFormat> sortedshoppingList = shoppingList.OrderBy(s => s.priority).ToList(); // Resort the list in case there have been any changes/if the print option has not been run
+                    foreach (var property in sortedshoppingList) // For every property in the sorted list
                     {
-                        if (property.boughtItem == true)
+                        if (property.boughtItem == true) // If the user has already bought the item
                         {
                             // Do nothing
                         }
-                        else
+                        else // If the user hasn't bought the item
                         {
-                            priceofItem = property.quantity * property.willingPrice;
-                            total += priceofItem;
+                            priceofItem = property.quantity * property.willingPrice; // Multiply the quantity by the price and store it in a variable
+                            total += priceofItem; // Add this variable onto the total
                         }
                     }
-                    Console.WriteLine("Total:");
+                    Console.WriteLine("Total:"); // Print the total
                     Console.WriteLine("-=-=-=");
                     Console.WriteLine("£" + total);
-                    Console.ReadLine();
-                    Console.Clear();
+                    Console.ReadLine(); // Wait for user input
+                    Console.Clear(); // Clear the console
                     
+                }
+                if (menuOption == "X") // If the user has chosen to exit
+                {
+                    Environment.Exit(9); // Exit with code 9
                 }
             }
         }
