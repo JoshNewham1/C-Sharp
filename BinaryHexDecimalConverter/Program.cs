b1df4f48fdb0f2224ba253768808ee79e54e168b
JoshNewham1 <joshnewham@live.com>
2017-07-12 07:13:18 +0100

    Version 1.2 - Implemented the two missing conversions that were not specified by the code challenge (Binary -> Decimal and Hexadecimal -> Binary)

--- BinaryHexDecimalConverter/Program.cs
+++ BinaryHexDecimalConverter/Program.cs
@@ -26,16 +26,19 @@ namespace BinaryHexDecimalConverter
             while (true) // Infinite loop
             {
                 Console.WriteLine("Would you like to convert:");
-                Console.WriteLine("Decimal to (B)inary");
-                Console.WriteLine("Decimal to (H)exadecimal");
-                Console.WriteLine("Binary to He(x)adecimal");
-                Console.WriteLine("Hexadecimal to (D)ecimal");
-                Console.WriteLine("(E)xit the program");
+                Console.WriteLine("(1) Decimal to Binary");
+                Console.WriteLine("(2) Decimal to Hexadecimal");
+                Console.WriteLine("(3) Binary to Hexadecimal");
+                Console.WriteLine("(4) Binary to Decimal");
+                Console.WriteLine("(5) Hexadecimal to Decimal");
+                Console.WriteLine("(6) Hexadecimal to Binary");
+                Console.WriteLine("(7) Exit the program");
 
                 menuOption = Console.ReadLine().ToUpper(); // Read the user's choice into a variable and capitalise it
 
-                while (menuOption == "B") // If the user wants to convert decimal to binary
+                while (menuOption == "1") // If the user wants to convert decimal to binary
                 {
+                    Console.Clear(); // Clears the console
                     isDecimal = false; // Reset the boolean to its default of 'false'
                     Console.WriteLine("Please specify a decimal or type 'M' to go back to the menu"); // Ask for the user input
                     userinputString = Console.ReadLine(); // Store it in a string
@@ -73,8 +76,9 @@ namespace BinaryHexDecimalConverter
 
                     }
                 }
-                while (menuOption == "H") // If the user chose 'H'
+                while (menuOption == "2") // If the user chosen to convert decimal to hexadecimal
                 {
+                    Console.Clear(); // Clears the console
                     isDecimal = false; // Reset the boolean to its default of 'false'
                     Console.WriteLine("Please specify a decimal or type 'M' to go back to the menu"); // Ask for the user input
                     userinputString = Console.ReadLine(); // Store it in a string
@@ -112,8 +116,9 @@ namespace BinaryHexDecimalConverter
 
                     }
                 }
-                while (menuOption == "X") // If the user has chosen 'X'
+                while (menuOption == "3") // If the user has chosen to convert binary to hexadecimal
                 {
+                    Console.Clear(); // Clears the console
                     isBinary = false; // Reset the boolean to its default of 'false'
                     Console.WriteLine("Please specify a binary number or type 'M' to go back to the menu"); // Ask for the user input
                     userinputString = Console.ReadLine(); // Store it in a string
@@ -149,8 +154,47 @@ namespace BinaryHexDecimalConverter
 
                     }
                 }
-                while (menuOption == "D") // If the user has chosen 'D'
+                while (menuOption == "4") // If the user has chosen to convert binary to decimal
                 {
+                    Console.Clear(); // Clears the console
+                    isBinary = false; // Reset the boolean to its default of 'false'
+                    Console.WriteLine("Please specify a binary number or type 'M' to go back to the menu"); // Ask for the user input
+                    userinputString = Console.ReadLine(); // Store it in a string
+                    if (userinputString == "M") // Check if that string is 'M' or 'm'
+                    {
+                        Console.Clear(); // If it is, clear the console
+                        menuOption = "M"; // Change the menuOption to break out of the loop
+                    }
+                    else // If the user types anything else
+                    {
+                        isBinary = Regex.IsMatch(userinputString, "^[01]+$"); // Assigns the boolean to true or false if it matches the pattern
+                        if (isBinary == true) // If the user's input matched the pattern (if the user's input contained only 1s and 0s)
+                        {
+                            userDecimal = Convert.ToInt64(userinputString, 2); // Converts the binary number into a 64-bit integer with Base-2
+                            Console.WriteLine("Decimal: " + userDecimal); // Writes the decimal value to the screen
+                            Console.ReadLine(); // Waits for user input
+                            Console.Clear(); // Clears the console
+                        }
+                        else // If the user types anything else
+                        {
+                            while (isBinary == false) // While the user's input is invalid
+                            {
+                                Console.WriteLine("Please enter a binary number."); // Tell the user to re-enter the binary number
+                                userinputString = Console.ReadLine(); // Read this into a variable
+                                isBinary = Regex.IsMatch(userinputString, "^[01]+$"); // Recheck this variable
+                            }
+                            // When the user enters a valid binary number:
+                            userDecimal = Convert.ToInt64(userinputString, 2); // Converts the binary number into a 64-bit integer with Base-2
+                            Console.WriteLine("Decimal: " + userDecimal); // Writes the decimal value to the screen
+                            Console.ReadLine(); // Waits for user input
+                            Console.Clear(); // Clears the console
+                        }
+
+                    }
+                }
+                while (menuOption == "5") // If the user has chosen to convert hexadecimal to decimal
+                {
+                    Console.Clear(); // Clears the console
                     isHex = false; // Reset the boolean to its default of 'false'
                     Console.WriteLine("Please specify a hexadecimal number or type 'M' to go back to the menu"); // Ask for the user input
                     userinputString = Console.ReadLine(); // Store it in a string
@@ -186,7 +230,45 @@ namespace BinaryHexDecimalConverter
 
                     }
                 }
-                if (menuOption == "E") // If the user has chosen 'E'
+                while (menuOption == "6")
+                {
+                    Console.Clear(); // Clears the console
+                    isHex = false; // Reset the boolean to its default of 'false'
+                    Console.WriteLine("Please specify a hexadecimal number or type 'M' to go back to the menu"); // Ask for the user input
+                    userinputString = Console.ReadLine(); // Store it in a string
+                    if (userinputString == "M") // Check if that string is 'M' or 'm'
+                    {
+                        Console.Clear(); // If it is, clear the console
+                        menuOption = "M"; // Change the menuOption to break out of the loop
+                    }
+                    else // If the user types anything else
+                    {
+                        isHex = Regex.IsMatch(userinputString, @"\A\b[0-9a-fA-F]+\b\Z"); // Assigns the boolean to true or false if it matches the pattern
+                        if (isHex == true) // If the user's input matched the pattern (if the user's input contained 0-9 and A-F)
+                        {
+                            userBinary = Convert.ToString(Convert.ToInt64(userinputString, 16), 2); ; // Converts the hex into an integer using Base-16 and then converting that back to a string using Base-2
+                            Console.WriteLine("Decimal: " + userBinary); // Writes the decimal value to the screen
+                            Console.ReadLine(); // Waits for user input
+                            Console.Clear(); // Clears the console
+                        }
+                        else // If the user types anything else
+                        {
+                            while (isHex == false) // While the user's input is invalid
+                            {
+                                Console.WriteLine("Please specify a hexadecimal number"); // Tell the user to re-enter the hexadecimal number
+                                userinputString = Console.ReadLine(); // Read this into a variable
+                                isHex = Regex.IsMatch(userinputString, @"\A\b[0-9a-fA-F]+\b\Z"); // Recheck this variable
+                            }
+                            // When the user enters a valid hexadecimal value:
+                            userBinary = Convert.ToString(Convert.ToInt64(userinputString, 16), 2); ; // Converts the hex into an integer using Base-16 and then converting that back to a string using Base-2
+                            Console.WriteLine("Decimal: " + userBinary); // Writes the decimal value to the screen
+                            Console.ReadLine(); // Waits for user input
+                            Console.Clear(); // Clears the console
+                        }
+
+                    }
+                }
+                if (menuOption == "7") // If the user has chosen to exit
                 {
                     Environment.Exit(9); // Exit the program safely with code 9
                 }
