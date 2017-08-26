febd0b1e4c49ebf9f494d01c84f8ccb41691743c
JoshNewham1 <joshnewham@live.com>
2017-08-26 14:32:37 +0100

    Version 1.0 - Implemented a simple menu and completed the first task without any input validation.

---
+++ ConditionalsTask/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menuInput;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-=-=-=-=-=-=");
+                Console.WriteLine("Conditionals");
+                Console.WriteLine("-=-=-=-=-=-=");
+                Console.WriteLine("Task (1)");
+                Console.WriteLine("Task (2)");
+                Console.WriteLine("Task (3)");
+                Console.WriteLine("Task (4)");
+                Console.WriteLine("E(x)it the program");
+                menuInput = Console.ReadLine().ToUpper();
+                if (menuInput == "1")
+                {
+                    Task1();
+                }
+                else if (menuInput == "2")
+                {
+
+                }
+                else if (menuInput == "3")
+                {
+
+                }
+                else if (menuInput == "4")
+                {
+
+                }
+                else if (menuInput == "X")
+                {
+                    Environment.Exit(9);
+                }
+            }
+        }
+        public static void Task1()
+        {
+            Console.WriteLine("Please enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            if (userNumber >= 1 && userNumber <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.ReadLine();
+        }
+    }
+}
