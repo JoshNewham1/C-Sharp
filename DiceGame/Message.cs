47989ec46a91668121e98ce7d15f728453cb95f1
JoshNewham1 <joshnewham@live.com>
2017-12-11 15:54:59 +0000

    Version 2.3 - Added multi-line comments and reduced variable declarations in Message.cs as suggested by sethsch1. Also renamed some methods and objects so they are more self-explanatory.

--- DiceGame/Message.cs
+++ DiceGame/Message.cs
@@ -9,19 +9,24 @@ namespace DiceGame
     class Message
     {
         public static string path = Environment.CurrentDirectory + @"\Data\"; // Define a string variable called path and set it to /bin/Debug/data (where the text files are stored)
-        public static string StartMessage() // StartMessage method
+        public static string GetStartMessage() // GetStartMessage method
         {
-            string messageText = File.ReadAllText(path + "welcomemessage.txt"); // Read in all the text from welcomemessage.txt
-            return messageText; // Return the text from messagetext.txt so it can be displayed
+            return File.ReadAllText(path + "welcomemessage.txt"); // Read in the text from welcomemessage.txt and return it
         }
-        public static string DoubleDiceMessage() // DoubleDiceMessage method
+        public static string GetDoubleDiceMessage() // GetDoubleDiceMessage method
         {
             string tempText = File.ReadAllText(path + "doubledicemessage.txt"); // Read in all the text from doubledicemessage.txt into a temporary variable
             if (tempText.Contains("*")) // If the text file contains an asterisk
             {
                 string diceTotal = (Library.GlobalVariables.diceValue1 + Library.GlobalVariables.diceValue2).ToString(); // Calculate the total of the two dice and store it in a variable
-                string messageText = tempText.Replace("*", diceTotal); // The text in the .txt file has an asterisk where the variable should be inserted, this code replaces the asterisk with the total of the dice
-                return messageText; // Return the replaced text so it can be displayed
+                return tempText.Replace("*", diceTotal);
+                /* The text in the .txt file has an asterisk 
+                 * where the variable should be inserted,
+                 * this code replaces the asterisk in the text
+                 * with the total of the two dice and
+                 * returns it to the main program
+                 */
+
             }
             else // If the text file doesn't contain an asterisk
             {
@@ -29,14 +34,22 @@ namespace DiceGame
             }
             
         }
-        public static string WinMessage() // WinMessage method
+        public static string GetWinMessage() // GetWinMessage method
         {
             string tempText = File.ReadAllText(path + "winmessage.txt"); // Read in all the text from winmessage.txt into a temporary variable
             if (tempText.Contains("*")) // If the text file contains an asterisk
             {
-                string totalTurns = Library.GlobalVariables.playerStats.GetValue(Library.GlobalVariables.currentPlayer, 2).ToString(); // Fetch the number of turns the current player has had from the playerStats array and store it in a variable
-                string messageText = tempText.Replace("*", totalTurns); // The text in the .txt file has an asterisk where the variable should be inserted, this code replaces the asterisk with the number of turns
-                return messageText; // Return the replaced text so it can be displayed
+                string totalTurns = Library.GlobalVariables.playerStats.GetValue(Library.GlobalVariables.currentPlayer, 2).ToString();
+                /* Fetch the number of turns the current player has taken
+                   from the playerStats array and store it in a variable */
+
+                return tempText.Replace("*", totalTurns); // Return the replaced text so it can be displayed
+                /* The text in the .txt file has an asterisk 
+                 * where the variable should be inserted,
+                 * this code replaces the asterisk in the text
+                 * with the total number of turns the player has taken
+                 * and returns it to the main program
+                 */
             }
             else // If the text file doesn't contain an asterisk
             {
